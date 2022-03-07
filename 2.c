ae9a86a4a331137e47bf695139cef72749fdecb8
MariaKlimenteva <100579299+MariaKlimenteva@users.noreply.github.com>
2022-03-07 20:24:57 +0300

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define n 4
+
+int main(){
+    int i;
+    char instruct[n];
+    char reg[n];
+    char num[n];
+    int out[n] = 0;
+    for (i = 0; i < n; i++){
+        
+        scanf(" %s %s %c ", &instruct[i], &reg[i], &num[i]);
+        
+        
+    }
+    
+    while (1){
+        if (instruct[1] = 'm'){
+            if ()
+            out[1] = 
+            break;
+        }
+        if (instruct[1] = 's'){
+            
+            break;
+        }
+        if (instruct[1] = 'a'){
+            
+            break;
+        }
+        else {
+            break;
+        }       
+    }
+}
+
+printf (' %d %d %d %d', out[n]); 
+return 0;
+}
