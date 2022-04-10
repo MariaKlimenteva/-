121bb335ff7d20046844f056adc91f58bc8d666f
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-04-10 15:39:25 +0300

    Не работает, но изменен

--- 2.c
+++ 2.c
@@ -1,40 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define n 4
+int i = 0;
+char instruct[n];
+char reg[n];
+char num[n];
+int smth;
+int out[n] = {0, 0, 0, 0};
 
-int main(){
-    int i;
-    char instruct[n];
-    char reg[n];
-    char num[n];
-    int out[n] = 0;
-    for (i = 0; i < n; i++){
-        
-        scanf(" %s %s %c ", &instruct[i], &reg[i], &num[i]);
-        
-        
-    }
+int main(){ 
     
+    printf(" Обозначайте регистры как a, b, c, d \n");
+
     while (1){
-        if (instruct[1] = 'm'){
-            if ()
-            out[1] = 
-            break;
+        scanf("%s %s %c", instruct, reg, num);//считываем то что ввели
+
+        if (num[0] - 48 >= 9)//то это регистр введен третьим
+            smth = out[counter(num)];
+        
+        else{//это число
+            for (int l = 0, l < 4, l++) smth = smth + num[1] - 48;    
         }
-        if (instruct[1] = 's'){
-            
-            break;
-        }
-        if (instruct[1] = 'a'){
-            
-            break;
-        }
-        else {
-            break;
-        }       
+        command(instruct);
     }
+    return 0;
 }
 
-printf (' %d %d %d %d', out[n]); 
-return 0;
+int counter(char *reg[n]){//обязательно ли *?
+    switch(reg[0]){
+        case 'a': i = 1;
+        case 'b': i = 2;
+        case 'c': i = 3;
+        case 'd': i = 4;
+        default: printf ("Введен несуществующий регистр")
+    }
+    return i;
 }
+
+int command(char *instruct[n]){
+    switch(instruct[0]){
+        case 'm': mov(out, *reg, smth)
+        case 'a': add(out, *reg, smth)
+        case 's': sub(out, *reg, smth)
+        case '0': printf("Выход"); return 0;
+        case 'r': printf("Выход"); return 0;
+    }
+}
+void mov (int out, char *reg, int smth){ //функция, которая ставит третье введенное число или регистр на место out[i], в зависимости от того, какой регистр был введен
+    int i = counter(reg);
+    out[i] = smth;
+    for (int k = 0, k < n, k++) printf("%d", out[k]);
+    return;
+}
+
+void add (int out, char *reg, int smth){
+    int i = counter(reg);
+    out[i] = out[i] + smth;
+    for (int k = 0, k < n, k++) printf("%d", out[k]);
+    return;
+}
+
+void sub (int out, char *reg, int smth){
+    int i = counter(reg);
+    out[i] = out[i] - smth;
+    for (int k = 0, k < n, k++) printf("%d", out[k]); 
+    return;   
+}
+
