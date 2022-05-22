0387ccbe35c2e38d7e317f7a69ab42b3f6b2ae94
Maria Klimenteva <klimenteva.mr@phystech.edu>
2022-05-23 01:02:06 +0300

    доделано

--- 1.B.c
+++ 1.B.c
@@ -1,179 +1,155 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Elem * root; // глобальная переменная, хранящая указатель на начало дерева
-struct Info { //отдельная структура для сбора нужной информации
-    struct Elem ** p_field; //указатель на поле внутри родительской структуры, хранящее указатель на удаляемый элемент
-    struct Elem * del_el;//удаляемый элемент
-    struct Elem * most_right;//максимальный эл-т левого поддерева
-    struct Elem * parent_most_right;
-}
-struct Elem {
-	int val; // значение, которое хранится в элементе ((!) может быть указателем на строку, например)
-	struct Elem * lv, * pr; // указатели на левого и правого потомков
+struct Node { //Полями структуры могут быть любые объявленные типы, кроме самой структуры этого же типа, но можно хранить указатель на структуру этого типа
+    int val; 
+    struct Node *left;
+    struct Node *right;
 };
-struct Elem * find_elem(int x){ // проход по дереву в этом случае аналогичен add_elem
 
-	if (root == NULL){
-		printf("Empty tree");
-		return NULL;
-	}
-	
-	struct Elem * p = root; // p - будет указателем на текущий элемент на каждой итерации
-	while(1){
-		if(x == p->val) // если значение совпало - мы нашли нужный элемент
-			return p;
-		if(x < p->val){ // если x меньше, идем искать в левом поддереве
-			if(p->lv != NULL)
-				p = p->lv; // если левое поддерево есть - переходим левого потомка
-			else
-				return NULL; // если левого поддерева нет - значит не нашли нужный элемент
-		}
-		if(x > p->val){ // если x больше, идем искать в правом поддереве
-			if(p->pr != NULL)
-				p = p->pr;
-			else
-				return NULL;
-		}
-	}
-	
-	return NULL;
-}
+struct Node * Insert(struct Node * tree, int x); 
+struct Node *Minimum(struct Node *tree); 
+struct Node *Search(struct Node *tree, int k);
+struct Node *parent_search(struct Node *tree, struct Node *Search); 
+void Transplant(struct Node *tree, struct Node *current, struct Node *changeTo);
+void Erase(struct Node *tree, struct Node *Find);
+void InOrder(struct Node *tree);
+void tree_destroy(struct Node * tree);
 
-void add_elem(int x){ // это нерекурсивная реализация, но можете и рекурсивную сделать тоже, по аналогии с print_tree_dfs
-	struct Elem * p = (struct Elem *)malloc(sizeof(struct Elem)); // выделить под элемент память
-	p->val = x; // заполнить его поля
-	p->lv = NULL;
-	p->pr = NULL;
-	
-	// ищем для него подходящее место в дереве
-	// если дерево пусто
-	if (root == NULL){
-		root = p;
-		return;
-	}
-	
-	// если дерево не пусто, ищем для него родительский элемент (вставляем только как лист)
-	struct Elem * q = root;
-	while(1){
-		if (x < q->val){ // если x меньше, чем значение текущего элемента, то вставляем в левое поддерево
-			if (q->lv != NULL){
-				q = q->lv; // переходим в левого потомка текущего элемента
-			}
-			else{
-				q->lv = p; // вставляем новый элемент, если левого потомка не было
-				break;
-			}
-		}
-		else{ // если x больше или равно, то - в правое поддерево
-			if (q->pr != NULL){
-				q = q->pr; // переход в правого потомка
-			}
-			else{
-				q->pr = p; // вставка в качестве правого потомка
-				break;
-			}
-		}
-	}
-
-	return;
-}
-// Функция нахождения самого правого элемента (сделала самостоятельно, не из примера)
-struct Elem * find_most_right (int x){//рассматриваем левое поддерево удаляемого элемента
-
-    struct Elem * p == x -> lv; // p - указатель на текущий элемент на каждой итерации
-    while(1){
-        if (p -> pr != NULL){
-            p = p -> pr;
-        }
-        else{
-            most_right == p;
-            return p;
-        }
+int main() {
+    struct Node * tree = NULL;
+    int x = 0;
+    printf("fill a binary tree\nType 0 to stop : \n");
+    scanf("%d", &x);
+    while (x != 0) {
+        tree = Insert(tree, x);
+        scanf("%d", &x);
     }
-}
-
-void print_tree_dfs(struct Elem * p){ // рекурсивный обход дерева в глубину
-	if (p == NULL){
-		return;
-	}
-	
-	print_tree_dfs(p->lv); // распечатать левое поддерево все целиком
-	printf("%d ", p->val); // распечатать значение текущего элемента
-	print_tree_dfs(p->pr); // распечатать правое поддерево все целиком
-	
-	return;
-}
-
-//Функция нахождения родителя удаляемого элемента (сделана самостоятельно, не из примера)
-struct Elem * find_parent (int x){ 
-
-    struct Elem * p == root; //p - указатель на текущий элемент на каждой итерации
-    if (x == p){ 
-     return NULL;
-    }
-    if (p -> lv == NULL) && (p -> pr == NULL){
-        return NULL;
-    }
-    else{
-        while (1){
-            if (p -> pr == x) || (p -> lv == x){
-                return p;
-            }
-            else{
-                if (x > p -> val){
-                    if (p -> pr != NULL){
-                        p = p -> pr;
-                    }
-                    else{
-                        return NULL;
-                    }
-                else{
-                    if (p -> lv != NULL){
-                        p = p -> lv;
-                    }
-                    else{
-                        return NULL;
-                    }
-                }
-                }
-            }
-        }   
-    }
-}   
-
-
-int main (){
-    root = NULL;
-	add_elem(7); // вставим в корневой элемент
-	add_elem(5); // пойдет как левый потомок 7
-	add_elem(8); // пойдет как правый потомок 7
-	add_elem(6); // пойдет как правый потомок 5
-	add_elem(3); // пойдет как левый потомок 5
-	add_elem(1); // пойдет как левый потомок 3
-	add_elem(4); // пойдет как правый потомок 3
-	add_elem(9); // пойдет как правый потомок 8
-    printf("Дерево:\n"); 
-	print_tree_dfs(root); 
-	printf("\n");
+    InOrder(tree);
+    printf("\n");
+    printf("Type element you want to erase:");
+    int k = 0;
+    scanf("%d", &k); // элемент который удаляем
+    printf("\n");
+    struct Node *Find = Search(tree, k);
+    Erase(tree, Find);
+    InOrder(tree);
+    tree_destroy(tree);
+    
     return 0;
 }
 
-////////// еще не доделала, проблема в сборе информации для структуры, хотя почитала как работать со структурами
-void del_elem(struct Info * del_el) { //функция удаления элемента (подаем в функцию значение удаляемого эл-та)
-if ((del_el -> lv) == NULL) {//нет левого потомка удаляемого эл-та
 
-    if ((del_el -> pr) == NULL) { //если и правого потомка нет
-       
-        del_el -> val == NULL;
-        * p_field == NULL;//?? так ли мы обNULLяем то, что в род эл-те про уд эл-т 
 
+struct Node * Insert(struct Node * tree, int x) {//рекурсивная реализация функции, которая вставляет элемент
+    if(tree == NULL) { //если нет дерева
+        struct Node * tree = (struct Node *)malloc(sizeof(struct Node)); //выделяем память под вставляемый элемент
+        tree->val = x; //значением корня делаем вставляемый элемент
+        tree->left = NULL; 
+        tree->right = NULL;
+        return tree;
     }
-    else{ //нет левого, но есть правый потомок
-        * p_field == del_el -> pr;           
-    }    
+    if(x < tree->val) {
+        tree->left = Insert(tree->left, x);
+        return tree; 
+    }
+    if(x > tree->val) {
+        tree->right = Insert(tree->right, x);
+        return tree;
+    }
+    return tree;
 }
-else{
-    if (find_parent(most_right) == )
-}   
 
+struct Node *Minimum(struct Node *tree) { // рекурсивная функция нахождение минимального - самого левого
+    if (tree->left == NULL) {
+        return tree;
+    }
+    return Minimum(tree->left);
+}
+
+struct Node* Search(struct Node *tree, int k) { //функция поиска элемента
+    if ((tree == NULL) || (k == tree->val)) { //возвращает корень если его нет или значение искомого это значение корня
+        return tree;
+    } 
+    if (k < tree->val) { //если значение меньше значения корня, идем в левое поддерево 
+        return Search(tree->left, k);
+    }
+    else {
+        return Search(tree->right, k); //иначе идем в правое поддерево
+    }
+}
+struct Node *parent_search(struct Node *tree, struct Node *Search) {
+
+    if ((tree->right == Search) || ((tree->left) == Search)) {
+        return tree; //если возвращаемое значение search это правый или левый потомок корня то возвращаем корень
+    }
+    else {
+        if (tree->val < Search->val) {
+            parent_search(tree->right, Search);
+        }
+        else {
+            parent_search(tree->left, Search);
+        }
+    }
+}
+
+void Transplant(struct Node *tree, struct Node *current, struct Node *changeTo) { //является функцией, которая привязывает указатели на след.элемент/родителя
+    struct Node *current_parent = parent_search(tree, current); //получаем структуру родителя элемента который надо заменить(current) и на что надо заменить (changeto)
+    struct Node *changeto_parent = parent_search(tree, changeTo);
+
+    if ( current == current_parent->left) { //пытаемся понять: current - левый или правый потомок?
+        current_parent->left = changeTo;
+    }
+    else {
+        current_parent->right = changeTo;
+    }
+    if (changeTo != NULL) {//Заменяемый элемент (changeto) вообще существует или это NULL? 
+        changeto_parent = current_parent;//Если существует, надо указатель на родителя(changeto_parent) сделать current_parent
+    }
+}//в результате этой функции, мы как бы вообще избавляемся от этого блока current (на него никто теперь не указывает и он ни на что не указывает)
+
+void Erase(struct Node *tree, struct Node *del) { 
+//Функция Erase это как бы Transplant, но рассматривающая разные случаи.
+//В отличии от Transplant, который просто меняет на элемент, который ему передадут, Erase должен понять, на что менять
+    if (del->left == NULL) {
+        Transplant(tree, del, del->right);
+    }
+    else if (del->right == NULL) {
+        Transplant(tree, del, del->left);
+    } 
+//строки 113 - 118 рассматривают простые случаи, когда один из потомков удаляемого элемента нулевой. Очевидно, в таком случае надо вызывать Transplant от другого.
+    else {
+        struct Node *y = Minimum(del->right); // y = указатель на наименьший элемент правого поддерева удаляемого элемента
+        struct Node *y_parent = parent_search(tree, y);
+        struct Node *y_left = parent_search(tree, y->left);
+        struct Node *y_right = parent_search(tree, y->right);
+        if (y_parent != del) {
+            Transplant(tree,y, y->right); //меняем y на правого потомка y
+            y->right = del->right;
+            y_right = y; 
+        }
+        Transplant(tree, del, y);
+        y->left =del->left;
+        y_left = y;
+    }
+/*строки 120 - 133 - рассматривается более сложный случай, когда потомки оба не нулевые =>
+ то есть надо выбрать тот, который либо самый левый (=минимальный) правого поддерева, либо самый правый(= максимальный) левого поддерева*/
+}
+
+
+
+void InOrder(struct Node *tree) {
+    if (tree != NULL) {
+        InOrder(tree->left);
+        printf("%d ", (int)tree->val);
+        InOrder(tree->right);
+    }
+}
+
+void tree_destroy(struct Node * tree) {
+    if(tree != NULL) {
+        tree_destroy(tree->left);
+
+        tree_destroy(tree->right);
+        free(tree);
+    }
 }
